fc300b3ba8094e98562e85d7ef637e438a22c7f0
lyubi <lyubi@DESKTOP-JOA3EEC>
2021-09-02 22:03:54 +0900

    시간 계산하기

---
+++ src/mathematics/Alarmclock.java
@@ -0,0 +1,30 @@
+package mathematics;
+
+import java.util.Scanner;
+
+public class Alarmclock {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int hh = sc.nextInt();
+		int mm = sc.nextInt();
+		
+		if(hh == 0 && mm >= 45) {
+			mm = mm-45;
+		} else if(hh == 0 && mm < 45) {
+			mm = mm+15;
+			hh = (hh+23);
+		} else if(hh != 0 && mm >= 45) {
+			mm = mm-45;
+		} else if(hh != 0 && mm < 45) {
+			mm = mm+15;
+			hh = hh-1;
+		} 
+		
+		System.out.printf("%d %d",hh, mm);		
+
+	}
+
+}
